d60639990ea6fefc0b550f0126d6a5c1a262b35d
rockchen <you@example.com>
2017-08-09 12:00:15 +0800

    add adaboost alg

---
+++ adaboost/test.py
@@ -0,0 +1,8 @@
+from sklearn.externals.six.moves import zip
+
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
